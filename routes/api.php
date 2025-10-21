30e728e3f12b582116dfb9d2d8346eeeb43a95e1
ibrahim <ibrahim.khalaf99@gmail.com>
2025-10-21 19:09:33 +0300

    add aggregation as api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\v1\TransactionReportApiController;
+use App\Services\AggregationService;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
     Route::get('/transaction-report', [TransactionReportApiController::class, 'index'])
         ->name('transaction-report');
+
+    Route::get('/aggregation', function (AggregationService $service) {
+        return response()->json($service->getAggregatedResults());
+    });
 });
